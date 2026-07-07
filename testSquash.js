5b2ca1d23ed0ccdf8255c53e7d1b41aa871a0e9a
noelfania <noelfania@gmail.com>
2026-07-07 18:16:37 +0900

    testSquash 2

--- testSquash.js
+++ testSquash.js
@@ -8,6 +8,14 @@ function testSquash() {
             input: [1, 2, 3, 4, 5],
             output: 15
         },
+        {
+            input: [1, 2, 3, 4, 5],
+            output: 15
+        },
+        {
+            input: [1, 2, 3, 4, 5],
+            output: 15
+        },
     ]
     for (const testCase of testCases) {
         const result = squash(testCase.input);
